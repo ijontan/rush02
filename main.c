23b7c1ce8f3e3d68f289f6d75944fcb3ac6ad258
ijon <itan@student.42kl.edu.my>
2022-09-03 11:05:50 +0800

    main

---
+++ main.c
@@ -0,0 +1,15 @@
+
+int	main(int argc, char const *argv[])
+{
+	if (!(argc == 2 || argc == 3))
+		return (0);
+	if (argc == 2)
+	{
+		/* code */
+	}
+	if (argc == 3)
+	{
+		/* code */
+	}
+	return (0);
+}
